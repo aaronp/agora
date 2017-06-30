360e24a22af9b2b565cb0ff3b3a138c32259fdc9
Aaron Pritzlaff <aaron.pritzlaff@gmail.com>
2017-06-30 12:54:20 +0100

    prepared gh-pages repo

--- build.sbt
+++
@@ -1,5 +0,0 @@
-name := MyBuild.NamePrefix + "root"
-
-version := "1.0"
-
-scalaVersion := "2.10.3"
--- project/Build.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-import Keys._
-
-object MyBuild extends Build {
-  val NamePrefix = "MyProject-"
-
-  println(new java.io.File(".").getAbsolutePath)
-}
