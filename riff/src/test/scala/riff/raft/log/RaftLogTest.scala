8d1ccebdfbc347db91ab50a481ca115c0fb19002
Aaron Pritzlaff <aaron.pritzlaff@gmail.com>
2018-07-31 23:45:46 +0100

    in memory tests

--- riff/src/test/scala/riff/raft/log/RaftLogTest.scala
+++ riff/src/test/scala/riff/raft/log/RaftLogTest.scala
@@ -5,7 +5,7 @@ import riff.raft.{LogCoords, LogState}
 
 class RaftLogTest extends RiffSpec {
 
-  "RaftLog.ForDir commit" should {
+  "RaftLog commit" should {
     "error when trying to commit when no entries are appended" in {
       withLog { log =>
         val exp = intercept[Exception] {
@@ -39,9 +39,97 @@ class RaftLogTest extends RiffSpec {
       }
     }
   }
-  "RaftLog.ForDir append" should {
+
+  "RaftLog append" should {
     "remove old appended entries if asked to append an earlier entry with a greater term" in {
       withLog { log =>
+        log.logState shouldBe LogState.Empty
+
+        log.append(LogCoords(4, 1), "a")
+        log.append(LogCoords(4, 2), "b")
+        log.append(LogCoords(4, 3), "c")
+
+        log.termForIndex(1) shouldBe Some(4)
+        log.termForIndex(2) shouldBe Some(4)
+        log.termForIndex(3) shouldBe Some(4)
+        log.latestAppended() shouldBe LogCoords(4, 3)
+
+        // call the method under test -- appending term 5 at index 2 should remove our previous entries
+        log.append(LogCoords(5, 2), "replacing entry")
+
+        log.latestAppended() shouldBe LogCoords(5, 2)
+        log.termForIndex(1) shouldBe Some(4)
+        log.termForIndex(2) shouldBe Some(5)
+        log.termForIndex(3) shouldBe None
+      }
+    }
+    "error if asked to skip a log entry" in {
+      withLog { log =>
+        log.logState shouldBe LogState.Empty
+
+        log.append(LogCoords(7, 1), "first")
+        val exp = intercept[Exception] {
+          log.append(LogCoords(7, 3), "bang")
+        }
+        exp.getMessage should include("Attempt to skip a log entry by appending LogCoords(7,3) when the latest entry was LogCoords(7,1)")
+      }
+    }
+    "error if asked to append the same entry with the same term" in {
+      withLog { log =>
+        log.logState shouldBe LogState.Empty
+
+        log.append(LogCoords(7, 1), "first")
+        val exp = intercept[Exception] {
+          log.append(LogCoords(7, 1), "bang")
+        }
+        exp.getMessage should include("Attempt to append LogCoords(7,1) when our latest term is LogCoords(7,1)")
+      }
+    }
+
+    "error if asked to append the same entry with an earlier term" in {
+      withLog { log =>
+        log.logState shouldBe LogState.Empty
+
+        log.append(LogCoords(7, 1), "first")
+        val exp = intercept[IllegalArgumentException] {
+          log.append(LogCoords(6, 1), "bang")
+        }
+        exp.getMessage should include("Attempt to append LogCoords(6,1) when our latest term is LogCoords(7,1)")
+
+      }
+    }
+    "increment the index on each append" in {
+      withLog { log =>
+        log.logState shouldBe LogState.Empty
+
+        log.termForIndex(0) shouldBe None
+        log.termForIndex(1) shouldBe None
+        log.logState shouldBe LogState.Empty
+
+        log.append(LogCoords(2, 1), "first entry")
+        log.logState shouldBe LogState(commitIndex = 0, latestTerm = 2, latestIndex = 1)
+        log.termForIndex(1) shouldBe Some(2)
+
+        log.append(LogCoords(2, 2), "first entry")
+        log.logState shouldBe LogState(commitIndex = 0, latestTerm = 2, latestIndex = 2)
+        log.termForIndex(2) shouldBe Some(2)
+
+        log.append(LogCoords(3, 3), "first entry")
+        log.logState shouldBe LogState(commitIndex = 0, latestTerm = 3, latestIndex = 3)
+        log.termForIndex(2) shouldBe Some(2)
+        log.termForIndex(3) shouldBe Some(3)
+
+        log.append(LogCoords(3, 4), "first entry")
+        log.logState shouldBe LogState(commitIndex = 0, latestTerm = 3, latestIndex = 4)
+        log.termForIndex(4) shouldBe Some(3)
+      }
+    }
+  }
+
+  "RaftLog.ForDir append" should {
+    "remove old appended entries if asked to append an earlier entry with a greater term" in {
+      withDir { dir =>
+        val log = RaftLog[String](dir)
         log.logState shouldBe LogState.Empty
 
         log.append(LogCoords(4, 1), "a") shouldBe LogAppendResult(log.dir.resolve("1.entry"))
@@ -68,70 +156,16 @@ class RaftLogTest extends RiffSpec {
         log.termForIndex(3) shouldBe None
       }
     }
-    "error if asked to skip a log entry" in {
-      withLog { log =>
-        log.logState shouldBe LogState.Empty
-
-        log.append(LogCoords(7, 1), "first") shouldBe LogAppendResult(log.dir.resolve("1.entry"))
-        val exp = intercept[Exception] {
-          log.append(LogCoords(7, 3), "bang")
-        }
-        exp.getMessage should include("Attempt to skip a log entry by appending LogCoords(7,3) when the latest entry was LogCoords(7,1)")
-      }
-    }
-    "error if asked to append the same entry with the same term" in {
-      withLog { log =>
-        log.logState shouldBe LogState.Empty
-
-        log.append(LogCoords(7, 1), "first") shouldBe LogAppendResult(log.dir.resolve("1.entry"))
-        val exp = intercept[Exception] {
-          log.append(LogCoords(7, 1), "bang")
-        }
-        exp.getMessage should include("Attempt to append LogCoords(7,1) when our latest term is LogCoords(7,1)")
-      }
-    }
-
-    "error if asked to append the same entry with an earlier term" in {
-      withLog { log =>
-        log.logState shouldBe LogState.Empty
-
-        log.append(LogCoords(7, 1), "first") shouldBe LogAppendResult(log.dir.resolve("1.entry"))
-        val exp = intercept[IllegalArgumentException] {
-          log.append(LogCoords(6, 1), "bang")
-        }
-        exp.getMessage should include("Attempt to append LogCoords(6,1) when our latest term is LogCoords(7,1)")
-
-      }
-    }
-    "increment the index on each append" in {
-      withLog { log =>
-        log.logState shouldBe LogState.Empty
-
-        log.termForIndex(0) shouldBe None
-        log.termForIndex(1) shouldBe None
-        log.logState shouldBe LogState.Empty
-
-        log.append(LogCoords(2, 1), "first entry") shouldBe LogAppendResult(log.dir.resolve("1.entry"))
-        log.logState shouldBe LogState(commitIndex = 0, latestTerm = 2, latestIndex = 1)
-        log.termForIndex(1) shouldBe Some(2)
-
-        log.append(LogCoords(2, 2), "first entry") shouldBe LogAppendResult(log.dir.resolve("2.entry"))
-        log.logState shouldBe LogState(commitIndex = 0, latestTerm = 2, latestIndex = 2)
-        log.termForIndex(2) shouldBe Some(2)
-
-        log.append(LogCoords(3, 3), "first entry") shouldBe LogAppendResult(log.dir.resolve("3.entry"))
-        log.logState shouldBe LogState(commitIndex = 0, latestTerm = 3, latestIndex = 3)
-        log.termForIndex(2) shouldBe Some(2)
-        log.termForIndex(3) shouldBe Some(3)
-
-        log.append(LogCoords(3, 4), "first entry") shouldBe LogAppendResult(log.dir.resolve("4.entry"))
-        log.logState shouldBe LogState(commitIndex = 0, latestTerm = 3, latestIndex = 4)
-        log.termForIndex(4) shouldBe Some(3)
-      }
-    }
   }
 
-  def withLog(test: FileBasedLog[String] => Unit) = withDir { dir => test(RaftLog[String](dir))
+  def withLog(test: RaftLog[String] => Unit) = withDir { dir =>
+    withClue("file based") {
+      test(RaftLog[String](dir))
+    }
+
+    withClue("in memory") {
+      test(RaftLog.inMemory[String]())
+    }
   }
 
 }
